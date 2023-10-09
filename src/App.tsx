b8f2a99fd8b3c2041859310d906f757fb4d6dc39
정용우 <jyw1341@gmail.com>
2023-10-09 14:57:05 +0900

    feat: 스플래쉬 기능 추가
    스플래쉬 기능을 추가하고 폰트 로드

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useCallback, useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          "LineSeedKR-Bd": require("./assets/fonts/LINESeedKR-Bd.otf"),
+          "LineSeedKR-Rg": require("./assets/fonts/LINESeedKR-Rg.otf"),
+          "LineSeedKR-Th": require("./assets/fonts/LINESeedKR-Th.otf"),
+        });
+        await Font.loadAsync(Ionicons.font);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      onLayout={onLayoutRootView}
+    >
+      <Text>SplashScreen Demo! 👋</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
